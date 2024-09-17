9ab28c9efa7afef93d2ced929ce58a9403edfb00
VaddyVG <vads1999@inbox.ru>
2024-09-17 18:28:10 +0300

    Add exception, other

--- main.py
+++ main.py
@@ -1,59 +1,66 @@
 import os
 import shutil
+import json
 
 
-# Dictionary for file categories
+# Downloading the configuration from the config.json file
 
-FILE_CATEGORIES = {
-    'Images': ['.jpg', '.jpeg', '.png', '.gif', '.bmp'],
-    'Videos': ['.mp4', '.mov', '.avi', '.mkv'],
-    'Documents': ['.pdf', '.docx', '.txt', '.xlsx', '.pptx'],
-    'Music': ['.mp3', '.wav', '.flac'],
-    'Archives': ['.zip', '.rar', '.tar', '.gz'],
-    'Scripts': ['.py', '.js', '.html', '.css']
-}
-
-
-# Function for sorting files by category
-
-def sort_files(directory):
+def load_congif(config_path='config.json'):
+    try:
+        with open(config_path, 'r') as config_file:
+            return json.load(config_file)
+    except FileNotFoundError:
+        print(f'Error: configuration file "{config_path}" not found')
+        return None
+    except json.JSONDecodeError:
+        print('Error: Invalid JSON format in the configuration file')
+        return None
+    
+    
+def sort_files(directory, config):
+    if not config:
+        print('Sorting cannot be performed without configuration')
+        return
+    
+    directories = config['directories']
+    
+    # Creating folders for each category from the configuration
+    for folder in directories.keys():
+        folder_path = os.path.join(directory, folder)
+        os.makedirs(folder_path, exist_ok=True)
+        
+    # Go through all the files in the directory
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
         
-        # Skip directroies
-        if os.path.isdir(file_path):
-            continue
-    
-        # Defining the file extension
-        _, file_extension = os.path.splitext(filename)
-    
-    # Going through the file categories
-        moved = False
-        for category, extension in FILE_CATEGORIES.items():
-            if file_extension.lower() in extension:
-                category_path = os.path.join(directory, category)
+        # If it's file
+        if os.path.isfile(file_path):
+            file_extension = filename.split('.')[-1].lower() # Get extension
             
-                # If the folder for the category does not exist, create it
-                if not os.path.exists(category_path):
-                    os.makedirs(category_path)
+            # Check category
+            for folder, extension in directories.items():
+                if file_extension in extension:
+                    destination = os.path.join(directory, folder, filename)
+                    try:
+                        shutil.move(file_path, destination)
+                        print(f'The {filename} has been moved in {folder}')
+                    except (shutil.Error, OSError) as e:
+                        print(f'File transfer error {filename}: {e}')
+                    break
                 
-                # Move the file to the appropriate folder
-                shutil.move(file_path, os.path.join(category_path, filename))
-                print(f"Moved {filename} to {category_path}")
-                moved = True
-                break
-    
-        # If the file was not moved to any category, move it to the 'Others' folder
-        if not moved:
-            other_path = os.path.join(directory, "Other")
-            if not os.path.exists(other_path):
-                os.makedirs(other_path)
-            shutil.move(file_path, os.path.join(other_path, filename))
-            print(f"Moved {filename} to {category_path}")
-        
-# Specify the path to the directory you want to sort
-directory_path = r"F:\vad_path"
+                
+# main function
 
-sort_files(directory_path)
+def main():
+    directory = input('Enter the path to the directory to sort: ')
     
-        
+    if not os.path.exists(directory):
+        print(f'Error: Directory "{directory}" not found')
+        return
+    
+    config = load_congif()
+    sort_files(directory, config)
+    
+
+if __name__ == "__main__":
+    main()
